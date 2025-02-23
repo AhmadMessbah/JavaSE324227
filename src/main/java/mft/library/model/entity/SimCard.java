d0d7ccec415773b50fa79b909db7e443e9cb4000
aryanmahshadi <aryanmahshadi80@gmail.com>
2025-02-23 20:19:43 +0330

    simcard updated

---
+++ src/main/java/mft/library/model/entity/SimCard.java
@@ -0,0 +1,15 @@
+package mft.library.model.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@SuperBuilder
+@ToString
+public class SimCard {
+}
---
+++ src/main/java/mft/library/model/entity/enums/SimCardType.java
@@ -0,0 +1,8 @@
+package mft.library.model.entity.enums;
+
+public enum SimCardType {
+    Irancell,
+    MCI,
+    Rightel,
+    Shatel
+}
