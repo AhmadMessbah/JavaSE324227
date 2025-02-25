bcfbc3d294b208ee49f666737469283f43f3aa6a
AmrSlmn <131772243+AmrSlmn@users.noreply.github.com>
2025-02-25 17:15:25 +0330

    DriverLicense updated

---
+++ src/main/java/mft/library/model/entity/enums/DriverLicenseType.java
@@ -0,0 +1,14 @@
+package mft.library.model.entity.enums;
+
+public enum DriverLicenseType {
+
+        CAR,
+        MOTORCYCLE,
+        TRUCK,
+        BUS,
+        TRACTOR,
+        BOAT,
+        AIRCRAFT
+
+}
+
---
+++ src/main/java/mft/library/model/service/DriverLicenseService.java
@@ -0,0 +1,69 @@
+package mft.library.model.service;
+
+import mft.library.model.entity.DriverLicense;
+import mft.library.model.repository.DriverLicenseRepository;
+
+import java.util.List;
+
+public class DriverLicenseService {
+
+    public static void save(DriverLicense driverLicense) throws Exception {
+        if (!(driverLicense.getDateTime().getYear() >= 2015 && driverLicense.getExpire().getYear() <= 2025)) {
+            throw new Exception("Invalid date");
+        }
+
+        try (DriverLicenseRepository driverLicenseRepository = new DriverLicenseRepository()) {
+            driverLicenseRepository.save(driverLicense);
+        }
+    }
+    
+    public static DriverLicense edit(DriverLicense driverLicense) throws Exception {
+        findById(driverLicense.getId());
+        if (!(driverLicense.getDateTime().getYear() >= 1980 && driverLicense.getExpire().getYear() <= 2020)) {
+            throw new Exception("Invalid date");
+        }
+        try (DriverLicenseRepository driverLicenseRepository = new DriverLicenseRepository()) {
+            driverLicenseRepository.edit(driverLicense);
+        }
+        return driverLicense;
+    }
+
+    public static void delete(int id) throws Exception {
+        findById(id);
+        try (DriverLicenseRepository driverLicenseRepository = new DriverLicenseRepository()) {
+            driverLicenseRepository.remove(id);
+        }
+    }
+
+    public static List<DriverLicense> findAll() throws Exception {
+        try (DriverLicenseRepository driverLicenseRepository = new DriverLicenseRepository()) {
+            List<DriverLicense> driverLicenses = driverLicenseRepository.findAll();
+            if (driverLicenses.isEmpty()) {
+                throw new Exception("No driver license found");
+            }
+            return driverLicenses;
+        }
+    }
+
+
+    public static DriverLicense findById(int id) throws Exception {
+        try (DriverLicenseRepository driverLicenseRepository = new DriverLicenseRepository()) {
+            DriverLicense driverLicense = driverLicenseRepository.findById(id);
+            if (driverLicense == null) {
+                throw new Exception("driver license not found");
+            }
+            return driverLicense;
+        }
+    }
+
+    public static List<DriverLicense> findByLicenseIDAndFamily(String licenseId, String family) throws Exception {
+        try (DriverLicenseRepository driverLicenseRepository = new DriverLicenseRepository()) {
+            List<DriverLicense> DriverLicenseList = driverLicenseRepository.findByLicenseIDAndFamily(licenseId, family);
+            if (DriverLicenseList.isEmpty()) {
+                throw new Exception("No driver license found");
+            }
+            return DriverLicenseList;
+        }
+    }
+
+}
